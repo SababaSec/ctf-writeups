06237ba21c601ce8596660c042237b0113b36c93
H. Z. Sababa <hb20007@users.noreply.github.com>
2019-10-10 11:51:39 +0300

    Update NACTF Phuzzy Photo Python script

--- 2019/NACTF/Forensics/Phuzzy Photo/solver.py
+++ 2019/NACTF/Forensics/Phuzzy Photo/solver.py
@@ -1,13 +1,16 @@
 from PIL import Image
 
-im = Image.open('pic.png')
-out = Image.new('I', im.size, 0)
+IMAGE_FILE = 'pic.png'
+
+im = Image.open(IMAGE_FILE)
+out = Image.new('I', im.size) # Image.new(mode, size, color=0): mode here is 'I' which has 32-bit signed integer pixels. The default image color is black so it will be initialized to that.
 
 width, height = im.size
+
 for x in range(width):
     for y in range(height):
         r, g, b, _ = im.getpixel((x, y))
         if y % 6 == 0 and x % 6 == 0 and (r < 255 or g < 255 or b < 255):
             out.putpixel((x, y), 0xffffff)
 
-out.save('pic2.png')
+out.save('new_pic.png')
